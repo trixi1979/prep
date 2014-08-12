c3ba5bffa441b3aff34a1fea5be3b9776d635fbf
Stefanie Brauers <stefanie.brauers@email.de>
2014-08-12 15:14:35 +0200

    Pushing new changes

---
+++ source/prep/sorting/Sort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using prep.matching_core;
+
+namespace prep.sorting
+{
+    public class Sort<T>
+    {
+        public IDontKnowYet by(IGetTheValueOfAnAttribute<T, AttributeType> accessor)
+        {
+            
+        }
+    }
+}
